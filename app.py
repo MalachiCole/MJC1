1c774e8bce741a0005da5c9deb0d74529842aae5
malachi.j.cole@gmail.com <malachi.j.cole@gmail.com>
2024-08-22 19:57:55 -0400

    Sychronizing Local

--- app.py
+++ app.py
@@ -888,10 +888,4 @@ while menu != exit_num:
         exit()
         menu = exit_num
     else:
-<<<<<<< HEAD
         error()
-=======
-        error()
-
-
->>>>>>> 92c9bd400162c84498e6cd056db76ae271f88fe8
