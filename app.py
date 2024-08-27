2ef00a9b9f9fe038675da18ad378a21c487e5c62
malachi.j.cole@gmail.com <malachi.j.cole@gmail.com>
2024-08-27 17:50:58 -0400

    Updating Cypher

--- app.py
+++ app.py
@@ -477,125 +477,35 @@ def swap_cases():
 
 
 def cypher():
+    lowercase = (
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z")
+    lowerdict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11,
+                 "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22,
+                 "x": 23, "y": 24, "z": 25}
     cypher1 = 0
     while cypher1 != 1:
         print("")
         print("Welcome to the Caesar Cypher, " + name_player + "!")
 
-        def translate(s):
+        def bettertranslate(s, shift):
             translation = ""
             for letter in s:
-                if letter in "a":
-                    translation = translation + "o"
-                elif letter in "b":
-                    translation = translation + "p"
-                elif letter in "c":
-                    translation = translation + "q"
-                elif letter in "d":
-                    translation = translation + "r"
-                elif letter in "e":
-                    translation = translation + "s"
-                elif letter in "f":
-                    translation = translation + "t"
-                elif letter in "g":
-                    translation = translation + "u"
-                elif letter in "h":
-                    translation = translation + "v"
-                elif letter in "i":
-                    translation = translation + "w"
-                elif letter in "j":
-                    translation = translation + "x"
-                elif letter in "k":
-                    translation = translation + "y"
-                elif letter in "l":
-                    translation = translation + "z"
-                elif letter in "m":
-                    translation = translation + "a"
-                elif letter in "n":
-                    translation = translation + "b"
-                elif letter in "o":
-                    translation = translation + "c"
-                elif letter in "p":
-                    translation = translation + "d"
-                elif letter in "q":
-                    translation = translation + "e"
-                elif letter in "r":
-                    translation = translation + "f"
-                elif letter in "s":
-                    translation = translation + "g"
-                elif letter in "t":
-                    translation = translation + "h"
-                elif letter in "u":
-                    translation = translation + "i"
-                elif letter in "v":
-                    translation = translation + "j"
-                elif letter in "w":
-                    translation = translation + "k"
-                elif letter in "x":
-                    translation = translation + "l"
-                elif letter in "y":
-                    translation = translation + "m"
-                elif letter in "z":
-                    translation = translation + "n"
-                elif letter in "A":
-                    translation = translation + "O"
-                elif letter in "B":
-                    translation = translation + "P"
-                elif letter in "C":
-                    translation = translation + "Q"
-                elif letter in "D":
-                    translation = translation + "R"
-                elif letter in "E":
-                    translation = translation + "S"
-                elif letter in "F":
-                    translation = translation + "T"
-                elif letter in "G":
-                    translation = translation + "U"
-                elif letter in "H":
-                    translation = translation + "V"
-                elif letter in "I":
-                    translation = translation + "W"
-                elif letter in "J":
-                    translation = translation + "X"
-                elif letter in "K":
-                    translation = translation + "Y"
-                elif letter in "L":
-                    translation = translation + "Z"
-                elif letter in "M":
-                    translation = translation + "A"
-                elif letter in "N":
-                    translation = translation + "B"
-                elif letter in "O":
-                    translation = translation + "C"
-                elif letter in "P":
-                    translation = translation + "D"
-                elif letter in "Q":
-                    translation = translation + "E"
-                elif letter in "R":
-                    translation = translation + "F"
-                elif letter in "S":
-                    translation = translation + "G"
-                elif letter in "T":
-                    translation = translation + "H"
-                elif letter in "U":
-                    translation = translation + "I"
-                elif letter in "V":
-                    translation = translation + "J"
-                elif letter in "W":
-                    translation = translation + "K"
-                elif letter in "X":
-                    translation = translation + "L"
-                elif letter in "Y":
-                    translation = translation + "M"
-                elif letter in "Z":
-                    translation = translation + "N"
+                if letter.isalpha():
+                    if letter.isupper():
+                        translation = translation + (lowercase[(lowerdict[letter.lower()] + int(shift)) % 26].upper())
+                    else:
+                        translation = translation + (lowercase[(lowerdict[letter] + int(shift)) % 26])
                 else:
                     translation = translation + letter
             return translation
 
+        shift = input("How many times would you like to shift the cypher? (1-26): ")
+
         if __name__ == '__main__':
+            print("")
             s = input("Enter a phrase: ")
-            result = translate(s)
+            result = bettertranslate(s, shift)
             print("")
             print("Translation: " + result)
 
